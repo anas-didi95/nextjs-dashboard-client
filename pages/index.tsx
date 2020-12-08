6a7eb00e68fbdf6b169dfdbba0a51ab2fd563adf
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-08 20:14:40 +0800

    Amend typing to es6 style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -60,3 +60,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
