cc247ca26331e06073a8cd0de34c77b2bf0a2c12
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-09 20:30:10 +0800

    Add handler for navbar menu button collapse

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,57 +1,64 @@
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 
-const Navbar = () => (
-  <nav
-    className="navbar is-spaced"
-    role="navigation"
-    aria-label="main navigation">
-    <div className="navbar-brand">
-      <Link href="/">
-        <a className="navbar-item" href="#">
-          <Image src="/images/dashboard.png" width={64} height={64} />
-          <p className="title is-4 ml-2">Dashboard</p>
+const Navbar = () => {
+  const [isActive, setActive] = useState(false)
+
+  const toggleActive = () => setActive(prev => !prev)
+
+  return (
+    <nav
+      className="navbar is-spaced"
+      role="navigation"
+      aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link href="/">
+          <a className="navbar-item" href="#">
+            <Image src="/images/dashboard.png" width={64} height={64} />
+            <p className="title is-4 ml-2">Dashboard</p>
+          </a>
+        </Link>
+        <a
+          role="button"
+          className={`navbar-burger burger ${isActive && "is-active"}`}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          onClick={toggleActive}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
-      </Link>
-      <a
-        role="button"
-        className="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-    <div id="navbarBasicExample" className="navbar-menu">
-      <div className="navbar-start">
-        <a className="navbar-item">Home</a>
-        <a className="navbar-item">Documentation</a>
-        <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link">More</a>
-          <div className="navbar-dropdown">
-            <a className="navbar-item">About</a>
-            <a className="navbar-item">Jobs</a>
-            <a className="navbar-item">Contact</a>
-            <hr className="navbar-divider" />
-            <a className="navbar-item">Report an issue</a>
+      </div>
+      <div id="navbarBasicExample" className={`navbar-menu ${isActive && "is-active"}`}>
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+          <a className="navbar-item">Documentation</a>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            <a className="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a className="button is-light">Log in</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-)
+    </nav>
+  )
+}
 
 export default Navbar
