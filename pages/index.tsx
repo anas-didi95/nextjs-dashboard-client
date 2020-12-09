30ba4bca28c7fbb1f7a5e32a1820dacc367e8aec
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-09 20:25:06 +0800

    Refactor navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,10 @@
 import React from "react"
-import Image from "next/image"
-import Link from "next/link"
+import Navbar from "../src/components/Navbar"
 
 const LoginPage = () => (
   <section className="hero is-primary is-fullheight is-info">
     <div className="hero-head">
-      <nav className="navbar is-spaced" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link href="/">
-            <a className="navbar-item" href="#">
-              <Image src="/images/dashboard.png" width={64} height={64} />
-              <p className="title is-4 ml-2">Dashboard</p>
-            </a>
-          </Link>
-          <a
-            role="button"
-            className="navbar-burger burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">Home</a>
-            <a className="navbar-item">Documentation</a>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
-            </div>
-          </div>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a className="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a className="button is-light">Log in</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
     </div>
     <div className="hero-body">
       <div className="container">
