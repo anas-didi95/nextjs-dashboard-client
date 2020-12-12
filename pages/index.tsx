0a02ae3286619623a9ab58bd4ce5acd8efdcca6d
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-12 20:54:48 +0800

    Refactor code

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,9 @@ import Form from "../src/components/Form"
 import FormInput from "../src/components/FormInput"
 import Navbar from "../src/components/Navbar"
 import AppLayout from "../src/layouts/AppLayout"
+import useConstants from "../src/utils/useConstants"
 
-const LoginPage = () => (
+const LoginPage: React.FC<{}> = () => (
   <AppLayout title="Login Page">
     <section className="hero is-primary is-fullheight is-info">
       <div className="hero-head">
@@ -17,17 +18,7 @@ const LoginPage = () => (
         <article className="container">
           <div className="columns is-centered">
             <div className="column is-5">
-              <Box>
-                <Form title="Sign In Form">
-                  <FormInput label="Username" type="text" />
-                  <FormInput label="Password" type="password" />
-                  <br />
-                  <ButtonGroup align="is-right">
-                    <Button label="Clear" type="button" color="is-light" isInverted isOutlined />
-                    <Button label="Sign In" type="submit" color="is-primary" />
-                  </ButtonGroup>
-                </Form>
-              </Box>
+              <SignInForm />
             </div>
           </div>
         </article>
@@ -35,5 +26,23 @@ const LoginPage = () => (
     </section>
   </AppLayout>
 )
+
+const SignInForm: React.FC<{}> = () => {
+  const constants = useConstants()
+
+  return (
+    <Box>
+      <Form title="Sign In Form">
+        <FormInput label={constants.label.username} type="text" />
+        <FormInput label={constants.label.password} type="password" />
+        <br />
+        <ButtonGroup align="is-right">
+          <Button label={constants.button.clear} type="button" color="is-light" isInverted isOutlined />
+          <Button label={constants.button.signIn} type="submit" color="is-primary" />
+        </ButtonGroup>
+      </Form>
+    </Box>
+  )
+}
 
 export default LoginPage
--- src/utils/useConstants.ts
+++ src/utils/useConstants.ts
@@ -5,7 +5,17 @@ const useConstants = () => {
       "Dashboard client application developed using Next.js and TypeScript.",
   }
 
-  return { metadata }
+  const label = {
+    username: "Username",
+    password: "Password"
+  }
+
+  const button = {
+    clear: "Clear",
+    signIn: "Sign In"
+  }
+
+  return { metadata, label, button }
 }
 
 export default useConstants
