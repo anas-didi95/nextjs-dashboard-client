73e530086d9a48e24fc1d3ddb7e636477ac681ec
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-16 20:11:07 +0800

    Run formatter

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -15,18 +15,18 @@ const FormInput: React.FC<IFormInput> = ({
   label,
   error,
 }) => (
-    <div className="field">
-      <label className="label">{label}</label>
-      <div className="control">
-        <input
-          className={`input ${!!error ? "is-danger" : "is-hovered"}`}
-          type={type}
-          name={name}
-          ref={register}
-        />
-      </div>
-      {!!error && <p className="help is-danger">{error}</p>}
+  <div className="field">
+    <label className="label">{label}</label>
+    <div className="control">
+      <input
+        className={`input ${!!error ? "is-danger" : "is-hovered"}`}
+        type={type}
+        name={name}
+        ref={register}
+      />
     </div>
-  )
+    {!!error && <p className="help is-danger">{error}</p>}
+  </div>
+)
 
 export default FormInput
