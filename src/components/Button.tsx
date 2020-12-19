d7d6d5c15b85b5d30a56373ae28e1867a6986a62
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-19 21:23:04 +0800

    Add animation for modal active

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -23,11 +23,9 @@ const Button: React.FC<IButton> = ({
   return (
     <button
       type={type}
-      className={`button ${!!color ? color : ""} ${
-        isOutlined ? "is-outlined" : ""
-      } ${isInverted ? "is-inverted" : ""} ${
-        loadingContext.isLoading() ? "is-loading" : ""
-      }`}
+      className={`button ${!!color ? color : ""} ${isOutlined ? "is-outlined" : ""
+        } ${isInverted ? "is-inverted" : ""} ${loadingContext.isLoading() ? "is-loading" : ""
+        }`}
       onClick={onClick}>
       {label}
     </button>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,7 +14,7 @@ const Modal: React.FC<IModal> = ({
 }) => (
   <div className={`modal ${isActive ? "is-active" : ""} px-4`}>
     <div className="modal-background" onClick={toggleActive}></div>
-    <div className="modal-card">
+    <div className={`modal-card ${isActive ? "animate__animated animate__zoomIn animate__faster" : ""}`}>
       <header className="modal-card-head">
         <p className="modal-card-title">{title}</p>
         <button
