d98aecd76801aed4af3c27cffa3320cf86a1f869
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-20 21:03:44 +0800

    Refactor constants

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -3,8 +3,10 @@ import Card from "../../src/components/Card"
 import LabelValue from "../../src/components/LabelValue"
 import AppLayout from "../../src/layouts/AppLayout"
 import DashboardLayout from "../../src/layouts/DashboardLayout"
+import useConstants from "../../src/utils/hooks/useConstants"
 
 const DashboardPage: React.FC<{}> = () => {
+  const constants = useConstants()
   const [currentTime, setCurrentTime] = useState("")
 
   useEffect(() => {
@@ -20,9 +22,9 @@ const DashboardPage: React.FC<{}> = () => {
   return (
     <AppLayout title="Home" needAuth={true}>
       <DashboardLayout breadcrumbs={["Home"]}>
-        <Card title="Welcome">
+        <Card title={constants.header.welcome}>
           <p className="title is-4">Hi, Anas Juwaidi</p>
-          <LabelValue label="Current time">
+          <LabelValue label={constants.label.currentTime}>
             <p>{currentTime}</p>
           </LabelValue>
         </Card>
