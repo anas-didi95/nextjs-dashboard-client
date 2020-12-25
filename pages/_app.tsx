e4b82f01362ef6a21be74a1374ea6f35fb4735de
Anas Juwaidi <anas.didi95@gmail.com>
2020-12-25 21:14:16 +0800

    Fix typescript error in strict mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import "../styles/app.scss"
 import { AuthProvider } from "../src/utils/contexts/AuthContext"
 import { NotificationProvider } from "../src/utils/contexts/NotificationContext"
 import { LoadingProvider } from "../src/utils/contexts/LoadingContext"
+import type { AppProps } from "next/app"
 
-const App = ({ Component, pageProps }) => (
+const App = ({ Component, pageProps }: AppProps) => (
   <AuthProvider>
     <NotificationProvider>
       <LoadingProvider>
