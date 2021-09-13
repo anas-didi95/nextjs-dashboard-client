d159ce9f74aad936a02c7fe5a49cd0dfcc33c5a8
Anas Juwaidi <anas.didi95@gmail.com>
2021-09-13 20:19:31 +0800

    ADD: Add form

--- pages/dashboard/security/user/[id]/change-password.tsx
+++ pages/dashboard/security/user/[id]/change-password.tsx
@@ -1,7 +1,10 @@
 import { useRouter } from "next/dist/client/router"
 import React from "react"
+import Button from "../../../../../src/components/Button"
 import ButtonGroup from "../../../../../src/components/ButtonGroup"
 import ButtonLink from "../../../../../src/components/ButtonLink"
+import Card from "../../../../../src/components/Card"
+import FormInput from "../../../../../src/components/FormInput"
 import AppLayout from "../../../../../src/layouts/AppLayout"
 import DashboardLayout from "../../../../../src/layouts/DashboardLayout"
 import useConstants from "../../../../../src/utils/hooks/useConstants"
@@ -17,7 +20,55 @@ const SecurityUserChangePassword: React.FC<{}> = () => (
 )
 
 const ChangePasswordCard: React.FC<{}> = () => {
-  return <div>Change password</div>
+  const constants = useConstants()
+
+  return (
+    <Card title={constants.header.changePassword}>
+      <form>
+        <div className="columns is-multiline is-variable is-4">
+          <div className="column is-6">
+            <FormInput
+              label={constants.label.oldPassword}
+              type="password"
+              register={null}
+            />
+          </div>
+          <div className="column is-hidden-mobile" />
+          <div className="column is-6">
+            <FormInput
+              label={constants.label.newPassword}
+              type="password"
+              register={null}
+            />
+          </div>
+          <div className="column is-6">
+            <FormInput
+              label={constants.label.confirmPassword}
+              type="password"
+              register={null}
+            />
+          </div>
+        </div>
+        <br />
+        <ButtonGroup align="is-right">
+          <Button
+            label={constants.button.clear}
+            type="button"
+            color="is-light"
+            isInverted
+            isOutlined
+            onClick={() => {}}
+          />
+          <Button
+            label={constants.button.changePassword}
+            type="submit"
+            color="is-success"
+            onClick={() => {}}
+          />
+        </ButtonGroup>
+      </form>
+    </Card>
+  )
 }
 
 const ActionButton: React.FC<{}> = () => {
