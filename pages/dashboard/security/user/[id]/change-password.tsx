9ca6c0e735b905f8a8c1f5982156fe79f1e7c481
Anas Juwaidi <anas.didi95@gmail.com>
2021-09-13 20:05:51 +0800

    ADD: Add action button

--- pages/dashboard/security/user/[id]/change-password.tsx
+++ pages/dashboard/security/user/[id]/change-password.tsx
@@ -1,13 +1,39 @@
+import { useRouter } from "next/dist/client/router"
 import React from "react"
+import ButtonGroup from "../../../../../src/components/ButtonGroup"
+import ButtonLink from "../../../../../src/components/ButtonLink"
 import AppLayout from "../../../../../src/layouts/AppLayout"
 import DashboardLayout from "../../../../../src/layouts/DashboardLayout"
+import useConstants from "../../../../../src/utils/hooks/useConstants"
 
 const SecurityUserChangePassword: React.FC<{}> = () => (
   <AppLayout title="Security - User Change Password" needAuth>
     <DashboardLayout breadcrumbs={["Security", "User", "Change Password"]}>
-      <div>Change password</div>
+      <ChangePasswordCard />
+      <br />
+      <ActionButton />
     </DashboardLayout>
   </AppLayout>
 )
+
+const ChangePasswordCard: React.FC<{}> = () => {
+  return <div>Change password</div>
+}
+
+const ActionButton: React.FC<{}> = () => {
+  const constants = useConstants()
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+    <ButtonGroup align="is-right">
+      <ButtonLink
+        label={constants.button.back}
+        color="is-primary"
+        href={`/dashboard/security/user/${id}`}
+      />
+    </ButtonGroup>
+  )
+}
 
 export default SecurityUserChangePassword
